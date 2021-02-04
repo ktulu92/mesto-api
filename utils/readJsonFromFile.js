68b9b3e11b230c116c61bd9c3363bd9af4c050ad
ktulu92 <sludgekicker@gmail.com>
2021-02-04 20:17:28 +0300

    bugfix2

--- utils/readJsonFromFile.js
+++ utils/readJsonFromFile.js
@@ -1,14 +1,16 @@
 const fs = require('fs').promises;
 
+const errorMessagePath = [{ message: 'Проверьте имя и путь файла!' }];
+const errorMessageJson = [{ message: 'Битый JSON!' }];
 const readJson = (path) => fs.readFile(path)
   .catch(() => {
-    throw [{ message: 'Проверьте имя и путь файла!' }];
+    throw errorMessagePath;
   })
   .then((text) => {
     try {
       return JSON.parse(text);
     } catch (_) {
-      throw [{ message: 'Битый JSON!' }];
+      throw errorMessageJson;
     }
   });
 module.exports = readJson;
